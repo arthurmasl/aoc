7e15a6eeca87b055891e5cde48171285dea02d47
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-14 16:57:32 +0200

    bench

--- src/2020/01/main.go
+++ src/2020/01/main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"aoc/src/internal/utils"
 )
 
 func main() {
-	lines := utils.GetLines("src/2020/01/input")
+	lines := utils.GetLines("src/2020/01/example")
 	nums := utils.ConvertToInts(lines)
 
-	for _, numA := range nums {
-		for _, numB := range nums {
-			for _, numC := range nums {
-				if numA+numB+numC == 2020 {
-					fmt.Println(numA * numB * numC)
-					return
+	t1 := time.Now()
+	solution2(nums)
+	fmt.Println(time.Since(t1))
+}
+
+func solution1(nums []int) int {
+	for _, a := range nums {
+		for _, b := range nums {
+			for _, c := range nums {
+				if a+b+c == 2020 {
+					return a * b * c
 				}
 			}
 		}
 	}
+
+	return -1
+}
+
+func solution2(nums []int) int {
+	for i, a := range nums {
+		for i2, b := range nums[i+1:] {
+			for _, c := range nums[i2+1:] {
+				if a+b+c == 2020 {
+					return a * b * c
+				}
+			}
+		}
+	}
+
+	return -1
 }
---
+++ src/2020/01/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"aoc/src/internal/utils"
+)
+
+var (
+	lines = utils.GetLines("./input")
+	nums  = utils.ConvertToInts(lines)
+)
+
+func Benchmark_solution1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		solution1(nums)
+	}
+}
+
+func Benchmark_solution2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		solution2(nums)
+	}
+}
