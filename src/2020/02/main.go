b5d8146e222d4b8d18d46759ba3a917914e25f7f
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-15 21:37:40 +0200

    ref

--- src/2020/02/main.go
+++ src/2020/02/main.go
@@ -8,43 +8,44 @@ import (
 	"aoc/src/internal/utils"
 )
 
-// func main() {
-// 	lines := utils.GetLines("src/2020/02/input")
-//
-// 	valid := 0
-//
-// 	for _, line := range lines {
-// 		chunks := strings.Split(line, " ")
-// 		numbers := strings.Split(chunks[0], "-")
-// 		n1, _ := strconv.Atoi(numbers[0])
-// 		n2, _ := strconv.Atoi(numbers[1])
-// 		letter := string(chunks[1][0])
-// 		word := chunks[2]
-//
-// 		letterCount := strings.Count(word, letter)
-//
-// 		fmt.Println(n1, n2, letterCount)
-//
-// 		if letterCount >= n1 && letterCount <= n2 {
-// 			valid++
-// 		}
-// 	}
-//
-// 	fmt.Println(valid)
-// }
-
 func main() {
 	lines := utils.GetLines("src/2020/02/input")
 
+	fmt.Println(part1(lines))
+	fmt.Println(part2(lines))
+}
+
+func parse(line string) (int, int, string, string) {
+	chunks := strings.Split(line, " ")
+	numbers := strings.Split(chunks[0], "-")
+	n1, _ := strconv.Atoi(numbers[0])
+	n2, _ := strconv.Atoi(numbers[1])
+	letter := string(chunks[1][0])
+	word := chunks[2]
+
+	return n1, n2, letter, word
+}
+
+func part1(lines []string) int {
 	valid := 0
 
 	for _, line := range lines {
-		chunks := strings.Split(line, " ")
-		numbers := strings.Split(chunks[0], "-")
-		n1, _ := strconv.Atoi(numbers[0])
-		n2, _ := strconv.Atoi(numbers[1])
-		letter := string(chunks[1][0])
-		word := chunks[2]
+		n1, n2, letter, word := parse(line)
+		letterCount := strings.Count(word, letter)
+
+		if letterCount >= n1 && letterCount <= n2 {
+			valid++
+		}
+	}
+
+	return valid
+}
+
+func part2(lines []string) int {
+	valid := 0
+
+	for _, line := range lines {
+		n1, n2, letter, word := parse(line)
 
 		fo := string(word[n1-1]) == letter
 		so := string(word[n2-1]) == letter
@@ -54,5 +55,5 @@ func main() {
 		}
 	}
 
-	fmt.Println(valid)
+	return valid
 }
