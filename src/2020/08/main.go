6fa46a8eaab1b5f1f1f6300c285115989aadb39c
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-29 21:51:43 +0200

    part 2

--- src/2020/08/main.go
+++ src/2020/08/main.go
@@ -16,40 +16,67 @@ var (
 )
 
 func main() {
-	lines := utils.GetLines("src/2020/08/example")
-	run(lines)
+	lines := utils.GetLines("src/2020/08/input")
+
+	for i := len(lines) - 1; i >= 0; i-- {
+		line := lines[i]
+		op := strings.Split(line, " ")[0]
+		if op == "acc" {
+			continue
+		}
+
+		var newLine string
+		if op == "jmp" {
+			newLine = strings.Replace(line, "jmp", "nop", 1)
+		}
+		if op == "nop" {
+			newLine = strings.Replace(line, "nop", "jmp", 1)
+		}
+
+		newLines := make([]string, len(lines))
+		copy(newLines, lines)
+		newLines[i] = newLine
+
+		acc, terminated := run(newLines)
+		if terminated {
+			fmt.Println(acc, terminated)
+			break
+		}
+	}
 }
 
-func run(lines []string) {
+func run(lines []string) (int, bool) {
+	terminated := false
 	ops := make(map[int]bool)
 	acc := 0
 	i := 0
 
-	for {
+	for i < len(lines) {
 		line := lines[i]
 		_, ok := ops[i]
 		if !ok {
 			ops[i] = true
 		} else {
-			fmt.Println("loop", i, line)
 			break
 		}
-
-		fmt.Println(i, line)
 
 		op, offsetStr, _ := strings.Cut(line, " ")
 		offset, _ := strconv.Atoi(offsetStr)
 
 		switch op {
-		case "nop":
-			i++
 		case "acc":
 			acc += offset
 			i++
 		case "jmp":
 			i += offset
+		case "nop":
+			i++
 		}
 	}
 
-	fmt.Println(acc)
+	if i >= len(lines) {
+		terminated = true
+	}
+
+	return acc, terminated
 }
