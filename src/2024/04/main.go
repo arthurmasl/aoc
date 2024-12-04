c3e2037e5a7a4b145954da8aee5115e7049f0fa8
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-04 14:59:56 +0200

    day 4

--- src/2024/04/main.go
+++ src/2024/04/main.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"aoc/src/internal/utils"
 )
 
+//  M M    S S    S M     M S       S M
+//   A      A      A       A         A    <-bad
+//  S S    M M    S M     M S       M S
+
 func main() {
-	lines := utils.GetLines("src/2024/04/example")
-	println(lines)
+	lines := utils.GetLines("src/2024/04/input")
+
+	matrix := make([][]string, len(lines))
+	for i, line := range lines {
+		matrix[i] = strings.Split(line, "")
+	}
+
+	total := 0
+
+	for y := range lines {
+		for x := range lines[y] {
+			if lines[y][x] == 'A' {
+				tl := safeGet(matrix, y-1, x-1)
+				tr := safeGet(matrix, y-1, x+1)
+				bl := safeGet(matrix, y+1, x-1)
+				br := safeGet(matrix, y+1, x+1)
+
+				if tl == br || tr == bl {
+					continue
+				}
+
+				hm := make(map[string]int)
+				hm[tl] += 1
+				hm[tr] += 1
+				hm[bl] += 1
+				hm[br] += 1
+
+				if hm["S"] == 2 && hm["M"] == 2 {
+					total++
+				}
+			}
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func safeGet(arr [][]string, row, col int) string {
+	if row >= 0 && row < len(arr) && col >= 0 && col < len(arr[row]) {
+		return arr[row][col]
+	}
+	return ""
 }
