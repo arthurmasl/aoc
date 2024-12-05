5332b4df07526caeedd8da9778f463eafaee0fae
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-05 15:40:54 +0200

    part 2

--- src/2024/05/main.go
+++ src/2024/05/main.go
@@ -30,26 +30,45 @@ func main() {
 	}
 
 	res := 0
+	incorrects := make([][]int, 0)
 
 PL:
 	for _, pages := range pageLines {
 		pageInts := utils.ConvertToInts(strings.Split(pages, ","))
 
 		for pageIndex, page := range pageInts {
-			// fmt.Println(page, rules[page])
-
 			for _, right := range rules[page] {
 				rightIndex := slices.Index(pageInts, right)
+
 				if rightIndex != -1 && rightIndex < pageIndex {
-					// fmt.Println("bad")
+					incorrects = append(incorrects, pageInts)
 					continue PL
 				}
 			}
 		}
 
-		// fmt.Println("correct")
-		res += pageInts[len(pageInts)/2]
 	}
 
-	fmt.Println("====", res)
+	for _, pages := range incorrects {
+		checkAndFix(pages, rules)
+		res += pages[len(pages)/2]
+	}
+
+	fmt.Println(res)
+}
+
+func checkAndFix(pages []int, rules map[int][]int) {
+PL:
+	for pageIndex, page := range pages {
+		for _, right := range rules[page] {
+			rightIndex := slices.Index(pages, right)
+
+			if rightIndex != -1 && rightIndex < pageIndex {
+				pages = slices.Delete(pages, pageIndex, pageIndex+1)
+				pages = slices.Insert(pages, rightIndex, page)
+				checkAndFix(pages, rules)
+				continue PL
+			}
+		}
+	}
 }
