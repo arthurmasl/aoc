519227286c2357c992b17d4a23b5ed0cf685bddc
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-06 14:40:28 +0200

    part 2

--- src/2024/06/main.go
+++ src/2024/06/main.go
@@ -10,20 +10,23 @@ type Vector struct {
 	x, y int
 }
 
-// 4987 to low
+// 758 to low
+// 2372 too high
 func main() {
-	lines := utils.GetLines("src/2024/06/example")
+	lines := utils.GetLines("src/2024/06/input")
 	w, h := len(lines[0]), len(lines)
 
 	visited := make(map[Vector]bool)
 
 	pos := Vector{}
+	startPos := Vector{}
 	vel := Vector{0, -1}
 
 	for y := range lines {
 		for x := range lines[y] {
 			if lines[y][x] == '^' {
 				pos = Vector{x: x, y: y}
+				startPos = Vector{x: x, y: y}
 				visited[pos] = true
 			}
 		}
@@ -33,7 +36,6 @@ func main() {
 		next := Vector{x: pos.x + vel.x, y: pos.y + vel.y}
 
 		if next.x < 0 || next.x >= w || next.y < 0 || next.y >= h {
-			fmt.Println("break")
 			break
 		}
 
@@ -45,9 +47,9 @@ func main() {
 		pos = next
 
 		// draw
-		newLine := []rune(lines[pos.y])
-		newLine[pos.x] = 'X'
-		lines[pos.y] = string(newLine)
+		// newLine := []rune(lines[pos.y])
+		// newLine[pos.x] = 'X'
+		// lines[pos.y] = string(newLine)
 
 		visited[pos] = true
 	}
@@ -55,5 +57,51 @@ func main() {
 	for _, line := range lines {
 		fmt.Println(line)
 	}
-	fmt.Println(len(visited))
+
+	loops := 0
+	for v := range visited {
+		if v == startPos {
+			continue
+		}
+
+		newLine := []rune(lines[v.y])
+		newLine[v.x] = '#'
+
+		newLines := make([]string, len(lines))
+		copy(newLines, lines)
+		newLines[v.y] = string(newLine)
+
+		found := scan(newLines, startPos)
+		if !found {
+			loops++
+		}
+	}
+
+	fmt.Println(len(visited), loops)
+}
+
+func scan(lines []string, pos Vector) bool {
+	w, h := len(lines[0]), len(lines)
+	vel := Vector{0, -1}
+
+	tries := 0
+	for {
+		tries++
+		if tries == 10000 {
+			return false
+		}
+
+		next := Vector{x: pos.x + vel.x, y: pos.y + vel.y}
+
+		if next.x < 0 || next.x >= w || next.y < 0 || next.y >= h {
+			return true
+		}
+
+		if lines[next.y][next.x] == '#' {
+			vel.x, vel.y = -vel.y, vel.x
+			continue
+		}
+
+		pos = next
+	}
 }
