2c0c0828db2966d6aca9b5a0141666ab7aaab84e
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-09 18:52:03 +0200

    day 9

--- src/2024/09/main.go
+++ src/2024/09/main.go
@@ -2,11 +2,89 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"slices"
+	"strconv"
 
 	"aoc/src/internal/utils"
 )
 
+type Item struct {
+	id int
+}
+
 func main() {
-	lines := utils.GetLines("src/2024/09/example")
-	fmt.Println(lines)
+	input, _ := os.ReadFile("src/2024/09/example")
+
+	storage := make([]Item, 0)
+	id := 0
+
+	for chunk := range slices.Chunk(input[:len(input)-1], 2) {
+		size, _ := strconv.Atoi(string(chunk[0]))
+		space := 0
+		if len(chunk) == 2 {
+			space, _ = strconv.Atoi(string(chunk[1]))
+		}
+		for range size {
+			storage = append(storage, Item{id})
+		}
+		for range space {
+			storage = append(storage, Item{-1})
+		}
+		id++
+	}
+
+	for _, item := range slices.Backward(storage) {
+		if item.id == -1 {
+			storage = slices.Delete(storage, len(storage)-1, len(storage))
+		} else {
+			break
+		}
+	}
+
+	// draw
+	for _, n := range storage {
+		if n.id == -1 {
+			fmt.Print(".")
+		} else {
+			fmt.Print(n.id)
+		}
+	}
+	fmt.Println()
+
+	for i, cell := range storage {
+		if cell.id == -1 {
+			for j, n := range slices.Backward(storage) {
+				if n.id != -1 {
+					storage[i].id = n.id
+					storage = slices.Delete(storage, j, j+1)
+					break
+				}
+			}
+
+			// draw
+			for _, n := range storage {
+				if n.id == -1 {
+					fmt.Print(".")
+				} else {
+					fmt.Print(n.id)
+				}
+			}
+			fmt.Println()
+		}
+	}
+
+	storage = utils.Filter(storage, func(v Item) bool {
+		return v.id != -1
+	})
+	sum := 0
+	for i, item := range storage {
+		if item.id == -1 {
+			continue
+		}
+		fmt.Println(i, id)
+		sum += i * item.id
+	}
+
+	fmt.Println(sum)
 }
