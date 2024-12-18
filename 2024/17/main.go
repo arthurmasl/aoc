baa59046a9bced47b931a724517c8f5ddf0342b9
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-18 20:26:01 +0200

    ref

--- 2024/17/main.go
+++ 2024/17/main.go
@@ -3,16 +3,37 @@ package main
 import (
 	"fmt"
 	"strconv"
+
+	"aoc/internal/utils"
+)
+
+const (
+	exampleInput  = 23999685
+	exampleOutput = 503576154
+	target        = 2411751503445530
 )
 
 func main() {
-	input := "23999685"
+	input := exampleInput
+	output := program(input)
 
+	fmt.Println("input ", input)
+	fmt.Println("output", output)
+	fmt.Println("target", target)
+	fmt.Println()
+
+	utils.Assert(program(exampleInput) == exampleOutput)
+	utils.Assert(program(reverseProgram(target)) == target)
+}
+
+func program(input int) int {
 	var a, b, c int
 	var result string
-	a, _ = strconv.Atoi(input)
 
-	for range len(input) + 1 {
+	a = input
+	digits := len(strconv.Itoa(input)) + 1
+
+	for range digits {
 		b = ((a & 7) ^ 1)
 		c = a >> b
 		b ^= 5
@@ -22,11 +43,26 @@ func main() {
 		result += strconv.Itoa(b & 7)
 	}
 
-	fmt.Println("target 2411751503445530")
-	fmt.Println("output", result)
-	fmt.Println(a, b, c)
+	res, _ := strconv.Atoi(result)
+	return res
+}
 
-	if result == "2411751503445530" {
-		panic("done")
+func reverseProgram(target int) int {
+	targetStr := strconv.Itoa(target)
+	digits := len(targetStr)
+
+	var a, b, c int
+
+	for i := digits - 1; i >= 0; i-- {
+		b, _ = strconv.Atoi(string(targetStr[i]))
+
+		c = (a >> (b ^ 1)) & 7
+		b ^= c
+		b ^= 5
+
+		a <<= 3
+		a |= (b & 7)
 	}
+
+	return a
 }
