330b036a6d7cae4bf1449c5033913449d44f10a4
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-18 12:56:31 +0200

    part 2

--- 2024/18/main.go
+++ 2024/18/main.go
@@ -9,15 +9,15 @@ import (
 	"aoc/internal/utils"
 )
 
-// const (
-// 	size  = 70
-// 	bytes = 1024
-// )
-
 const (
-	size  = 6
-	bytes = 12
+	size  = 70
+	bytes = 1024
 )
+
+// const (
+// 	size  = 6
+// 	bytes = 12
+// )
 
 type Vector struct {
 	x, y int
@@ -26,71 +26,78 @@ type Vector struct {
 var directions = []Vector{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
 
 func main() {
-	walls := utils.GetLines("example")[:bytes]
-	grid := make([]string, size+1)
+	allWalls := utils.GetLines("input")
+	i := bytes
 
-	start := Vector{0, 0}
-	end := Vector{size, size}
+	for {
+		i++
 
-	for y := range grid {
-		newRow := strings.Repeat(".", size+1)
-		grid[y] = newRow
-	}
+		walls := utils.GetLines("input")[:i]
+		grid := make([]string, size+1)
 
-	for _, wall := range walls {
-		xStr, yStr, _ := strings.Cut(wall, ",")
-		x, _ := strconv.Atoi(xStr)
-		y, _ := strconv.Atoi(yStr)
+		start := Vector{0, 0}
+		end := Vector{size, size}
 
-		newRow := []byte(grid[y])
-		newRow[x] = '#'
-		grid[y] = string(newRow)
-	}
-
-	path := make([]Vector, 0)
-
-	visited := make(map[Vector]bool)
-	visited[start] = true
-	parent := make(map[Vector]Vector)
-
-	queue := list.New()
-	queue.PushBack(start)
-
-	for queue.Len() > 0 {
-		node := queue.Front()
-		current := node.Value.(Vector)
-		queue.Remove(node)
-
-		if current == end {
-			path = make([]Vector, 0)
-			for current != start {
-				path = append(path, current)
-				current = parent[current]
-			}
-
-			fmt.Println("found", len(path))
-			continue
+		for y := range grid {
+			newRow := strings.Repeat(".", size+1)
+			grid[y] = newRow
 		}
 
-		for _, neighbor := range getNeighbors(grid, current) {
-			if !visited[neighbor] {
-				queue.PushBack(neighbor)
-				visited[neighbor] = true
-				parent[neighbor] = current
+		for _, wall := range walls {
+			xStr, yStr, _ := strings.Cut(wall, ",")
+			x, _ := strconv.Atoi(xStr)
+			y, _ := strconv.Atoi(yStr)
+
+			newRow := []byte(grid[y])
+			newRow[x] = '#'
+			grid[y] = string(newRow)
+		}
+
+		path := make([]Vector, 0)
+
+		visited := make(map[Vector]bool)
+		visited[start] = true
+		parent := make(map[Vector]Vector)
+
+		queue := list.New()
+		queue.PushBack(start)
+
+		for queue.Len() > 0 {
+			node := queue.Front()
+			current := node.Value.(Vector)
+			queue.Remove(node)
+
+			if current == end {
+				path = make([]Vector, 0)
+				for current != start {
+					path = append(path, current)
+					current = parent[current]
+				}
+
+				fmt.Println("found", len(path), allWalls[i])
+				break
+			}
+
+			for _, neighbor := range getNeighbors(grid, current) {
+				if !visited[neighbor] {
+					queue.PushBack(neighbor)
+					visited[neighbor] = true
+					parent[neighbor] = current
+				}
 			}
 		}
-	}
 
-	for _, pos := range path {
-		newRow := []byte(grid[pos.y])
-		newRow[pos.x] = 'O'
-		grid[pos.y] = string(newRow)
+		// for _, pos := range path {
+		// 	newRow := []byte(grid[pos.y])
+		// 	newRow[pos.x] = 'O'
+		// 	grid[pos.y] = string(newRow)
+		// 	// }
+		// }
+
+		// for _, row := range grid {
+		// 	fmt.Println(row)
 		// }
 	}
-
-	// for _, row := range grid {
-	// 	fmt.Println(row)
-	// }
 }
 
 func getNeighbors(grid []string, pos Vector) []Vector {
