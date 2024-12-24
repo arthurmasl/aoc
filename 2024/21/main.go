3944835ae6b705be9dcf5bab56ac015e33e799e7
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-25 00:35:35 +0200

    paths

--- 2024/21/main.go
+++ 2024/21/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strconv"
-	"strings"
 	"unicode/utf8"
 
 	"aoc/internal/utils"
@@ -32,9 +30,6 @@ var (
 	}
 )
 
-// 133676 to high
-// 132532
-// 132312 to low
 func main() {
 	inputs := utils.GetLines("input")
 
@@ -47,10 +42,10 @@ func main() {
 		code, _ := strconv.Atoi(input[:len(input)-1])
 		total += code * utf8.RuneCountInString(seq3)
 
-		fmt.Println(input)
-		fmt.Println(seq1)
-		fmt.Println(seq2)
-		fmt.Println(seq3)
+		// fmt.Println(input)
+		// fmt.Println(seq1)
+		// fmt.Println(seq2)
+		// fmt.Println(seq3)
 
 		fmt.Println(utf8.RuneCountInString(seq3), code)
 	}
@@ -59,69 +54,26 @@ func main() {
 	// utils.Assert(total == 126384)
 }
 
+type keys struct {
+	from rune
+	to   rune
+}
+
 func getSequence(grid []string, initialPos vec, input string) string {
 	seq := ""
 	pos := initialPos
 
 	for _, targetKey := range input {
-		targetPos := getPos(grid, targetKey)
-		moves := getMoves(grid, pos, targetPos)
+		key := getKey(grid, pos)
 
+		moves := paths[keys{key, targetKey}]
 		seq += moves + "A"
+
+		targetPos := getPos(grid, targetKey)
 		pos = targetPos
 	}
 
 	return seq
-}
-
-func getMoves(grid []string, pos, targetPos vec) string {
-	moves := make([]rune, 0)
-	deltaX := targetPos.x - pos.x
-	deltaY := targetPos.y - pos.y
-
-	keys := ""
-
-	moveX(grid, deltaX, &moves, &pos, &keys)
-	moveY(grid, deltaY, &moves, &pos, &keys)
-
-	hasBlank := strings.Contains(keys, "_")
-	if hasBlank {
-		slices.Reverse(moves)
-	}
-
-	return string(moves)
-}
-
-func moveX(grid []string, deltaX int, moves *[]rune, pos *vec, keys *string) {
-	if deltaX < 0 {
-		for range deltaX * -1 {
-			*moves = append(*moves, '<')
-			pos.x -= 1
-			*keys += string(getKey(grid, vec{pos.x, pos.y}))
-		}
-	} else {
-		for range deltaX {
-			*moves = append(*moves, '>')
-			pos.x += 1
-			*keys += string(getKey(grid, vec{pos.x, pos.y}))
-		}
-	}
-}
-
-func moveY(grid []string, deltaY int, moves *[]rune, pos *vec, keys *string) {
-	if deltaY < 0 {
-		for range deltaY * -1 {
-			*moves = append(*moves, '^')
-			pos.y -= 1
-			*keys += string(getKey(grid, vec{pos.x, pos.y}))
-		}
-	} else {
-		for range deltaY {
-			*moves = append(*moves, 'v')
-			pos.y += 1
-			*keys += string(getKey(grid, vec{pos.x, pos.y}))
-		}
-	}
 }
 
 func getKey(grid []string, pos vec) rune {
@@ -138,4 +90,137 @@ func getPos(grid []string, key rune) vec {
 	}
 
 	return vec{}
+}
+
+var paths = map[keys]string{
+	{'A', '0'}: "<",
+	{'0', 'A'}: ">",
+	{'A', '1'}: "^<<",
+	{'1', 'A'}: ">>v",
+	{'A', '2'}: "<^",
+	{'2', 'A'}: "v>",
+	{'A', '3'}: "^",
+	{'3', 'A'}: "v",
+	{'A', '4'}: "^^<<",
+	{'4', 'A'}: ">>vv",
+	{'A', '5'}: "<^^",
+	{'5', 'A'}: "vv>",
+	{'A', '6'}: "^^",
+	{'6', 'A'}: "vv",
+	{'A', '7'}: "^^^<<",
+	{'7', 'A'}: ">>vvv",
+	{'A', '8'}: "<^^^",
+	{'8', 'A'}: "vvv>",
+	{'A', '9'}: "^^^",
+	{'9', 'A'}: "vvv",
+	{'0', '1'}: "^<",
+	{'1', '0'}: ">v",
+	{'0', '2'}: "^",
+	{'2', '0'}: "v",
+	{'0', '3'}: "^>",
+	{'3', '0'}: "<v",
+	{'0', '4'}: "^<^",
+	{'4', '0'}: ">vv",
+	{'0', '5'}: "^^",
+	{'5', '0'}: "vv",
+	{'0', '6'}: "^^>",
+	{'6', '0'}: "<vv",
+	{'0', '7'}: "^^^<",
+	{'7', '0'}: ">vvv",
+	{'0', '8'}: "^^^",
+	{'8', '0'}: "vvv",
+	{'0', '9'}: "^^^>",
+	{'9', '0'}: "<vvv",
+	{'1', '2'}: ">",
+	{'2', '1'}: "<",
+	{'1', '3'}: ">>",
+	{'3', '1'}: "<<",
+	{'1', '4'}: "^",
+	{'4', '1'}: "v",
+	{'1', '5'}: "^>",
+	{'5', '1'}: "<v",
+	{'1', '6'}: "^>>",
+	{'6', '1'}: "<<v",
+	{'1', '7'}: "^^",
+	{'7', '1'}: "vv",
+	{'1', '8'}: "^^>",
+	{'8', '1'}: "<vv",
+	{'1', '9'}: "^^>>",
+	{'9', '1'}: "<<vv",
+	{'2', '3'}: ">",
+	{'3', '2'}: "<",
+	{'2', '4'}: "<^",
+	{'4', '2'}: "v>",
+	{'2', '5'}: "^",
+	{'5', '2'}: "v",
+	{'2', '6'}: "^>",
+	{'6', '2'}: "<v",
+	{'2', '7'}: "<^^",
+	{'7', '2'}: "vv>",
+	{'2', '8'}: "^^",
+	{'8', '2'}: "vv",
+	{'2', '9'}: "^^>",
+	{'9', '2'}: "<vv",
+	{'3', '4'}: "<<^",
+	{'4', '3'}: "v>>",
+	{'3', '5'}: "<^",
+	{'5', '3'}: "v>",
+	{'3', '6'}: "^",
+	{'6', '3'}: "v",
+	{'3', '7'}: "<<^^",
+	{'7', '3'}: "vv>>",
+	{'3', '8'}: "<^^",
+	{'8', '3'}: "vv>",
+	{'3', '9'}: "^^",
+	{'9', '3'}: "vv",
+	{'4', '5'}: ">",
+	{'5', '4'}: "<",
+	{'4', '6'}: ">>",
+	{'6', '4'}: "<<",
+	{'4', '7'}: "^",
+	{'7', '4'}: "v",
+	{'4', '8'}: "^>",
+	{'8', '4'}: "<v",
+	{'4', '9'}: "^>>",
+	{'9', '4'}: "<<v",
+	{'5', '6'}: ">",
+	{'6', '5'}: "<",
+	{'5', '7'}: "<^",
+	{'7', '5'}: "v>",
+	{'5', '8'}: "^",
+	{'8', '5'}: "v",
+	{'5', '9'}: "^>",
+	{'9', '5'}: "<v",
+	{'6', '7'}: "<<^",
+	{'7', '6'}: "v>>",
+	{'6', '8'}: "<^",
+	{'8', '6'}: "v>",
+	{'6', '9'}: "^",
+	{'9', '6'}: "v",
+	{'7', '8'}: ">",
+	{'8', '7'}: "<",
+	{'7', '9'}: ">>",
+	{'9', '7'}: "<<",
+	{'8', '9'}: ">",
+	{'9', '8'}: "<",
+	{'<', '^'}: ">^",
+	{'^', '<'}: "v<",
+	{'<', 'v'}: ">",
+	{'v', '<'}: "<",
+	{'<', '>'}: ">>",
+	{'>', '<'}: "<<",
+	{'<', 'A'}: ">>^",
+	{'A', '<'}: "v<<",
+	{'^', 'v'}: "v",
+	{'v', '^'}: "^",
+	{'^', '>'}: "v>",
+	{'>', '^'}: "<^",
+	{'^', 'A'}: ">",
+	{'A', '^'}: "<",
+	{'v', '>'}: ">",
+	{'>', 'v'}: "<",
+	{'v', 'A'}: "^>",
+	{'A', 'v'}: "<v",
+	{'>', 'A'}: "^",
+	{'A', '>'}: "v",
 }
